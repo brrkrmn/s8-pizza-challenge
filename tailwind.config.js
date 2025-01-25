4bcc9a8321c148a7fd050e366254a2ab41d47dca
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-01-25 17:19:38 +0300

    add colors and fonts to tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      roboto: ['"Roboto Condensed"', 'sans-serif'],
+      barlow: ['"Barlow"', 'sans-serif'],
+      quattrocento: ['"Quattrocento"', 'serif'],
+      satisfy: ['"Satisfy"', 'cursive'],
+    },
+    screens: {
+      mobile: "425px",
+      tablet: "768px",
+      laptop: "1024px",
+    },
+    colors: {
+      transparent: 'transparent',
+      'yellow': '#FDC913',
+      'red': '#CE2829',
+      'beige': '#FAF7F2',
+      'gray': {
+        'light': '#5F5F5F',
+        'dark': '#292929'
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
