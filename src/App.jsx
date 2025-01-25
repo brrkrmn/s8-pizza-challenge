8ff0f364f6e27fb26d7acc2d2f9f0942af6c7cc0
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-01-26 01:48:32 +0300

    style home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header/Header";
 import Home from "@/components/Home/Home";
 import OrderForm from "@/components/OrderForm/OrderForm";
 import Success from "@/components/Success/Success";
@@ -6,10 +5,9 @@ import { Route, BrowserRouter as Router, Routes } from "react-router";
 
 const App = () => {
   return (
-    <main className="w-screen h-full flex flex-col items-center justify-center px-4 mobile:px-7">
+    <main className="w-screen h-screen flex flex-col items-center justify-center">
       <Router>
-        <Header />
-        <body className="w-full max-w-[532px] h-screen">
+        <body className="w-full h-full flex items-start justify-center">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/order" element={<OrderForm />} />
