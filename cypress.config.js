c283df6b654b74d41039259bd1619f8834813cb3
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-01-30 02:10:44 +0300

    add script to view test coverage

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
+import codeCoverageTask from "@cypress/code-coverage/task.js";
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      codeCoverageTask(on, config);
+      return config;
     },
   },
 });
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    istanbul({
+      include: "src/**/*",
+      exclude: ["node_modules", "test"],
+      extension: [".js", ".ts", ".vue", ".jsx", ".tsx"],
+    }),
   ],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-})
+});
