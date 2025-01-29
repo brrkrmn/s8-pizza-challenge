aed024d92a6861f3385b3555c8088bfa476ef609
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-01-29 15:29:08 +0300

    add validation messages to form

--- src/components/Order/components/OrderForm/OrderForm.jsx
+++ src/components/Order/components/OrderForm/OrderForm.jsx
@@ -78,7 +78,7 @@ const OrderForm = ({ setOrderDetails }) => {
         className="w-full font-barlow max-w-[532px] h-full text-gray-medium py-10 flex flex-col items-start justify-start gap-5"
       >
         <div className="w-full flex items-start justify-between gap-10">
-          <fieldset className="flex flex-col items-start justify-start gap-4">
+          <fieldset className="flex flex-col items-start justify-start">
             <legend className="text-xl font-semibold text-gray-medium">
               Boyut Seç <span className="text-red">*</span>
             </legend>
@@ -102,6 +102,13 @@ const OrderForm = ({ setOrderDetails }) => {
                 </label>
               ))}
             </div>
+            <p
+              className={`text-red transition ${
+                !values.size ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              Boyut seçin
+            </p>
           </fieldset>
           <fieldset className="flex flex-col items-start justify-start h-full w-full">
             <label
@@ -126,16 +133,23 @@ const OrderForm = ({ setOrderDetails }) => {
                 </option>
               ))}
             </select>
+            <p
+              className={`text-red transition ${
+                !values.thickness ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              Hamur seçin
+            </p>
           </fieldset>
         </div>
-        <fieldset className="!mt-6 flex flex-col items-start justify-center gap-3">
+        <fieldset className="flex flex-col items-start justify-center gap-3 py-6">
           <h3 className="text-xl font-semibold text-gray-medium">
             Ek Malzemeler
           </h3>
           <p className="text-gray-light font-[400]">
             En fazla 10 malzeme seçebilirsiniz. 5₺
           </p>
-          <div className="flex items-center justify-start flex-wrap gap-4 py-10">
+          <div className="flex items-center justify-start flex-wrap gap-4 pt-3">
             {EXTRAS.map((extra) => (
               <label
                 className={`${
@@ -188,6 +202,18 @@ const OrderForm = ({ setOrderDetails }) => {
               </label>
             ))}
           </div>
+          <p
+            className={`text-red transition ${
+              values.extras.length < 4 || values.extras.length === 10
+                ? "opacity-100"
+                : "opacity-0"
+            }`}
+          >
+            {values.extras.length < 4
+              ? "En az 4 malzeme seçin"
+              : values.extras.length === 10 &&
+                "En fazla 10 malzeme seçebilirsiniz"}
+          </p>
         </fieldset>
         <fieldset className="flex flex-col items-start justify-center gap-3 w-full">
           <label
@@ -203,9 +229,16 @@ const OrderForm = ({ setOrderDetails }) => {
             name="userName"
             value={values.userName}
             onChange={handleChange}
-            className="border-1 w-full py-3 border-divider px-2 rounded-sm"
+            className="bg-beige focus:outline-1 outline-yellow w-full py-3 border-divider px-2 rounded-sm"
             placeholder="Siparişi teslim alacak kişinin adı"
           />
+          <p
+            className={`text-red transition ${
+              values.userName.length < 3 ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            En az 3 karakter girin"
+          </p>
         </fieldset>
         <fieldset className="flex flex-col items-start justify-center gap-3 w-full">
           <label
@@ -220,22 +253,11 @@ const OrderForm = ({ setOrderDetails }) => {
             name="note"
             value={values.note}
             onChange={handleChange}
-            className="border-1 w-full py-3 border-divider px-2 rounded-sm resize-y min-h-[80px]"
+            className="bg-beige focus:outline-1 outline-yellow w-full py-3 border-divider px-2 rounded-sm resize-y min-h-[80px]"
           />
         </fieldset>
         <hr className="border-t-1 border-gray-light opacity-50 w-full !my-4"></hr>
-        <div className="border-1 border-divider rounded-sm w-full p-10 flex flex-col items-start justify-center gap-3 font-semibold">
-          <h4 className="text-xl text-gray-medium">Sipariş Toplamı</h4>
-          <div className="flex items-center justify-between w-full text-gray-light">
-            <p>Seçimler</p>
-            <p>{values.extras.length * 5}₺</p>
-          </div>
-          <div className="flex items-center justify-between w-full text-red">
-            <p>Toplam</p>
-            <p>{values.totalPrice}₺</p>
-          </div>
-        </div>
-        <div className="w-full h-full flex items-center justify-center gap-4">
+        <div className="w-full flex items-start justify-start gap-2">
           <div className="w-fit flex items-center justify-center gap-0">
             <button
               type="button"
@@ -246,7 +268,7 @@ const OrderForm = ({ setOrderDetails }) => {
                   quantity: prev.quantity - 1,
                 }))
               }
-              className="w-12 h-12 bg-yellow text-gray-medium !font-semibold rounded-l-sm disabled:bg-divider disabled:cursor-auto cursor-pointer"
+              className="w-12 h-12 bg-yellow text-gray-medium !font-semibold rounded-l-sm disabled:bg-divider disabled:cursor-not-allowed cursor-pointer"
             >
               -
             </button>
@@ -261,19 +283,32 @@ const OrderForm = ({ setOrderDetails }) => {
                   quantity: prev.quantity + 1,
                 }))
               }
-              className="bg-yellow text-gray-medium !font-semibold py-3 rounded-r-sm w-12 h-12 disabled:bg-divider disabled:cursor-auto cursor-pointer"
+              className="bg-yellow text-gray-medium !font-semibold py-3 rounded-r-sm w-12 h-12 disabled:bg-divider disabled:cursor-not-allowed cursor-pointer"
             >
               +
             </button>
           </div>
-          <button
-            data-testid="submit-button"
-            disabled={isDisabled}
-            type="submit"
-            className="w-full bg-yellow disabled:bg-divider rounded-sm text-gray-medium !font-semibold px-6 py-3 cursor-pointer disabled:cursor-auto"
-          >
-            SİPARİŞ VER
-          </button>
+          <div className="w-full">
+            <div className="border-1 bg-beige border-divider rounded-sm w-full p-10 flex flex-col items-start justify-center gap-3 font-semibold">
+              <h4 className="text-xl text-gray-medium">Sipariş Toplamı</h4>
+              <div className="flex items-center justify-between w-full text-gray-light">
+                <p>Seçimler</p>
+                <p>{values.extras.length * 5}₺</p>
+              </div>
+              <div className="flex items-center justify-between w-full text-red">
+                <p>Toplam</p>
+                <p>{values.totalPrice}₺</p>
+              </div>
+            </div>
+            <button
+              data-testid="submit-button"
+              disabled={isDisabled}
+              type="submit"
+              className="w-full bg-yellow disabled:bg-divider rounded-sm text-gray-medium !font-semibold px-6 py-3 cursor-pointer disabled:cursor-not-allowed"
+            >
+              SİPARİŞ VER
+            </button>
+          </div>
         </div>
       </form>
     </div>
